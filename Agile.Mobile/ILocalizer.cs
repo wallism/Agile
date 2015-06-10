5b97881761cdb6c965223bf54863685ffd07eab9
wallism <mark@agiledevelopment.com.au>
2015-06-11 00:01:02 +1000

    created ILocalizer

---
+++ Agile.Mobile/ILocalizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agile.Mobile
+{
+    /// <summary>
+    /// Implement to return localized strings from AppResource manager
+    /// </summary>
+    public interface ILocalizer
+    {
+        string Get(string name);
+    }
+}
