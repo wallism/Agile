f10c1f5562fb757d5b49b9e23a1e7940035c4b85
wallism <mark@agiledevelopment.com.au>
2015-08-25 00:22:55 +1000

    improved cache

--- Framework/Agile.Framework.Server/Cache.cs
+++ Framework/Agile.Framework.Server/Cache.cs
@@ -2,38 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Caching;
-using System.Text;
-using System.Threading.Tasks;
 using Agile.Diagnostics.Logging;
-using Agile.Shared;
 
 namespace Agile.Framework.Server
 {
     public interface ICache
     {
-        T Get<T>(string key) 
+        void Remove<T>(string key)
             where T : class;
 
-        IList<T> GetCachedAll<T>(Func<IList<T>> loadFromDbFunc, int expiryMins = 30) 
+        T GetCached<T>(string key, Func<T> loadDataFunc = null, int expiryMins = 60)
             where T : class;
 
-        T GetCachedItemFromAll<T>(Func<T, bool> matcher, Func<IList<T>> loadFromDbFunc)
+        IList<T> GetCachedAll<T>(Func<IList<T>> loadDataFunc, int expiryMins = 30)
+            where T : class;
+
+        T GetCachedItemFromAll<T>(Func<T, bool> matcher, Func<IList<T>> loadDataFunc, int expiryMins = 60)
             where T : class;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>taken from: https://github.com/wallism/Agile/blob/master/Framework/Agile.Framework.Server/Cache.cs</remarks>
     public class Cache : ICache
     {
-
-        public T Get<T>(string key) 
-            where T : class
+        public void Remove<T>(string key) where T : class
         {
-            return MemoryCache.Default.Get(key) as T;
+            Logger.Debug("Cache.Remove: {0}", key);
+            if (!MemoryCache.Default.Contains(key))
+                return;
+
+            MemoryCache.Default.Remove(key);
         }
 
-        public T GetCachedItemFromAll<T>(Func<T, bool> matcher, Func<IList<T>> loadFromDbFunc)
+        private readonly object padlock = new object();
+
+        public T GetCached<T>(string key, Func<T> loadDataFunc = null, int expiryMins = 60)
             where T : class
         {
-            var all = GetCachedAll(loadFromDbFunc);
+            lock (padlock)
+            {
+                var cached = MemoryCache.Default.Get(key) as T;
+                if (cached != null)
+                    return cached;
+
+                // whatever is calling doesn't want (or know) how to load
+                if (loadDataFunc == null)
+                    return null;
+
+                try
+                {
+                    // load from db
+                    var loaded = loadDataFunc();
+                    if (loaded == null)
+                        return null;
+                    Logger.Debug("[{0}] Adding new {1} to cache", key, typeof(T).Name);
+                    MemoryCache.Default.Add(key, loaded, DateTimeOffset.Now.AddMinutes(expiryMins));
+
+                    return loaded;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "GetCached: {0}", typeof(T).Name);
+                    return null;
+                }
+            }
+        }
+
+
+        public T GetCachedItemFromAll<T>(Func<T, bool> matcher, Func<IList<T>> loadDataFunc, int expiryMins = 60)
+            where T : class
+        {
+            var all = GetCachedAll(loadDataFunc, expiryMins);
             if (all == null || !all.Any())
             {
                 Logger.Warning("GetCachedAll has 0 items for key:{1}", typeof(T).FullName);
@@ -45,7 +86,7 @@ namespace Agile.Framework.Server
         /// <summary>
         /// Gets items from cache, if not found, loads using the given func.
         /// </summary>
-        public IList<T> GetCachedAll<T>(Func<IList<T>> loadFromDbFunc = null, int expiryMins = 60) 
+        public IList<T> GetCachedAll<T>(Func<IList<T>> loadDataFunc = null, int expiryMins = 60)
             where T : class
         {
             var listKey = string.Format("List:{0}", typeof(T).FullName);
@@ -53,18 +94,18 @@ namespace Agile.Framework.Server
             if (cached != null && cached.Any())
                 return cached;
 
-            // whatever is calling doesn't want or know how to load
-            if(loadFromDbFunc == null) 
+            // whatever is calling doesn't want (or know) how to load
+            if (loadDataFunc == null)
                 return null;
 
             try
             {
                 // load from db
-                var loaded = loadFromDbFunc();
-                if (loaded == null || !loaded.Any()) 
+                var loaded = loadDataFunc();
+                if (loaded == null || !loaded.Any())
                     return new List<T>();
                 Logger.Debug("Adding {0} new {1} to cache", loaded.Count, typeof(T).Name);
-                MemoryCache.Default.Add(listKey, loaded, AgileDateTime.Now.AddMinutes(expiryMins));
+                MemoryCache.Default.Add(listKey, loaded, DateTimeOffset.Now.AddMinutes(expiryMins));
 
                 return loaded;
             }
